3880595864fea32cd59564c16131825fc6bf73bd
MonkeyMax123 <98365388+MonkeyMax123@users.noreply.github.com>
2022-04-15 13:09:40 -0500

    Ready for submission

--- answers.md
+++ answers.md
@@ -1,7 +1,8 @@
 # CMPS 2200 Recitation 7
 ## Answers
 
-**Name:**_________________________
+**Name:**
+Max Curl
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
@@ -10,17 +11,19 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 - **d.**
 
-File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
-----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
+| File         | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length |
+|--------------|---------------------|----------------|--------------------------|
+| f1.txt       | 1340                | 826            | 0.616                    |
+| alice29.txt  | 1039367             | 676374         | 0.651                    | 
+| asyoulik.txt | 876253              | 606448         | 0.692                    | 
+| grammar.lsp  | 26047               | 17356          | 0.666                    | 
+| fields.c     | 78050               | 56206          | 0.720                    | 
+|              |                     |                | avg: 0.669               |                               
 
-
+The consistent trend is that the huffman encoding is always smaller than the fixed length coding and on average it is about 2/3 the size.
 
 
 - **e.**
 
+The expected cost for each letter is consistent across all documents given that the frequencies are all equal.  For example, if we used every letter in the English alphabet then the encoding would either be 4 or 5 bits.  Regardless of the frequencies though the encoding remains the same.  The total cost of the document would still be variable based on the frequencies of each letter.
 
--- main.py
+++ main.py
@@ -1,61 +1,84 @@
 import math, queue
 from collections import Counter
 
+
 class TreeNode(object):
     # we assume data is a tuple (frequency, character)
     def __init__(self, left=None, right=None, data=None):
         self.left = left
         self.right = right
         self.data = data
+
     def __lt__(self, other):
-        return(self.data < other.data)
+        return (self.data < other.data)
+
     def children(self):
-        return((self.left, self.right))
-    
+        return ((self.left, self.right))
+
+
 def get_frequencies(fname):
     ## This function is done.
-    ## Given any file name, this function reads line by line to count the frequency per character. 
-    f=open(fname, 'r')
+    ## Given any file name, this function reads line by line to count the frequency per character.
+    f = open(fname, 'r')
     C = Counter()
     for l in f.readlines():
         C.update(Counter(l))
-    return(dict(C.most_common()))
+    return (dict(C.most_common()))
 
-# given a dictionary f mapping characters to frequencies, 
+
+# given a dictionary f mapping characters to frequencies,
 # create a prefix code tree using Huffman's algorithm
 def make_huffman_tree(f):
     p = queue.PriorityQueue()
     # construct heap from frequencies, the initial items should be
     # the leaves of the final tree
     for c in f.keys():
-        p.put(TreeNode(None,None,(f[c], c)))
+        p.put(TreeNode(None, None, (f[c], c)))
 
     # greedily remove the two nodes x and y with lowest frequency,
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
-        
+        x = p.get()
+        y = p.get()
+        z_val = x.data[0] + y.data[0]
+        p.put(TreeNode(x, y, (z_val, "")))
+
     # return root of the tree
     return p.get()
 
+
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+    if node.left is None and node.right is None:
+        code[node.data[1]] = prefix
+    if node.left is not None:
+        get_code(node.left, prefix + '0')
+    if node.right is not None:
+        get_code(node.right, prefix + '1')
+    return code
+
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
+    x = get_frequencies(f)
+    bits = math.ceil(math.log2(len(x.keys())))
+    cost = bits * sum(x.values())
+    return cost
+
 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
-    # TODO
-    pass
+    counter = 0
+    for element in f:
+        counter += f[element] * len(C[element])
+    return counter
 
-f = get_frequencies('f1.txt')
-print("Fixed-length cost:  %d" % fixed_length_cost(f))
+
+f = get_frequencies('alice29.txt')
+print(f)
+print("Fixed-length cost:  %d" % fixed_length_cost('alice29.txt'))
 T = make_huffman_tree(f)
 C = get_code(T)
+print(C)
 print("Huffman cost:  %d" % huffman_cost(C, f))
